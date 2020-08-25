e4922f44254390e5f4f4ff849ad38251aa041edc
maha <maha@gmail.com>
2020-08-25 15:58:02 +0530

    Hybrid day-3

--- Salesforce/src/test/java/com/project/Salesforce/BaseClass/BaseTest.java
+++ Salesforce/src/test/java/com/project/Salesforce/BaseClass/BaseTest.java
@@ -6,12 +6,16 @@ import java.util.Properties;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import com.project.Salesforce.utilities.ExcelAPI;
+
 public class BaseTest 
 {
 	public  String projectPath=System.getProperty("user.dir");
 	public FileInputStream fis = null;
 	public  Properties parentProp = null;
 	public  Properties childProp = null;
+	public  Properties orProp =null;
+	public ExcelAPI xls;
 	
 	
 	public void load() throws Exception 
@@ -27,6 +31,23 @@ public class BaseTest
 		childProp.load(fis);
 		String url = childProp.getProperty("zohourl");
 		System.out.println(url);
+		
+		fis = new FileInputStream(projectPath+ "//src//test//resources//or.properties");
+		orProp = new Properties();
+		orProp.load(fis);
+		
+		//How do i come to know which suite to initiate?
+		String[] pack = this.getClass().getPackage().getName().split("\\.");
+		String suiteName = pack[pack.length-1];
+		System.out.println(suiteName);
+		
+		//init the testName
+		String testName = this.getClass().getSimpleName();
+		System.out.println(testName);
+		
+		
+		//init the xls file
+		xls = new ExcelAPI(childProp.getProperty(suiteName+"_xls")); 
 		
 	}
 	
